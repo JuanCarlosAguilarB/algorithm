4326bb40b3746c2f748a54097e70a6fd16c0d059
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2023-05-07 23:20:50 -0500

    add another version of solve

--- isAlienLanguageProblem.js
+++ isAlienLanguageProblem.js
@@ -69,3 +69,37 @@ const order3 = "abcdefghijkmnopqrstuvwxyz";
 console.log(isAlienSorted(words3, order3), "Output falseds");
 // Output:
 // false
+
+// _________________________________________________________________________________________________________________________
+// another version of solution
+
+var isAlienSorted = function (palabras, orden) {
+  // Crear mapa del diccionario alienigena
+  mapa_diccionario = new Map();
+  for (let i = 0; i < orden.length; i++) {
+    mapa_diccionario[orden[i]] = i;
+  }
+
+  // Revisar el orden de las palabras
+  for (let i = 1; i < palabras.length; i++) {
+    // T = 0(n)   S = 0(m)
+    if (comparar(palabras[i - 1], palabras[i]) === false) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// O(longitud de la palabra mÃ¡s larga)
+var comparar = function (palabra1, palabra2) {
+  const longitud = Math.min(palabra1.length, palabra2.length);
+  for (let i = 0; i < longitud; i++) {
+    if (mapa_diccionario[palabra1[i]] < mapa_diccionario[palabra2[i]]) {
+      return true;
+    }
+    if (mapa_diccionario[palabra1[i]] > mapa_diccionario[palabra2[i]]) {
+      return false;
+    }
+  }
+  return palabra1.length <= palabra2.length;
+};
